829891400fab45df71fb232dbfa1f36dfea2eed3
Magitian <magitian@duck.com>
2025-01-15 22:52:53 +0530

    Add basic implementation of server and client side message transfer - yet to add colouring

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -2,23 +2,32 @@ use std::net::{TcpListener, TcpStream};
 use std::io::Read;
 use std::env;
 use std::str;
-use std::thread;
+
+fn encrypt_xoxo(msg: &mut u64, v: &Vec<u64>) -> u64 {
+	for i in 0..v.len() {
+		*msg = (*msg) ^ v[i]
+	}
+	*msg
+}
 
 fn handle_client(mut stream: TcpStream) {
     let mut buffer = [0u8; 512];
-    
+    let mut enc_msg_xoxo: &str = "";
     // First, read the encrypted message as a binary string
     match stream.read(&mut buffer) {
         Ok(size) => {
             if let Ok(message) = str::from_utf8(&buffer[..size]) {
+				enc_msg_xoxo = message;
                 // Convert the received binary string back into an integer
-                let encrypted_message = u64::from_str_radix(message, 2).unwrap_or(0);
-                println!(">>> Encrypted message received: {}", encrypted_message);
+                // let encrypted_message = u64::from_str_radix(message, 2).unwrap_or(0);
+                // println!(">>> Encrypted message received: {}", encrypted_message);
+                println!(">>> Encrypted message received: {}", message);
             } else {
                 eprintln!("Failed to decode encrypted message");
             }
         }
         Err(e) => eprintln!("Failed to read encrypted message from stream: {}", e),
+
     }
 
     // Read the primes (serialized u64 integers)
@@ -32,12 +41,18 @@ fn handle_client(mut stream: TcpStream) {
                     .collect();
                 
                 println!(">>> Received primes: {:?}", primes);
+
+				let mut enc_msg_xoxo2 = u64::from_str_radix(enc_msg_xoxo, 2).unwrap();
+				let dec_msg_xoxo = encrypt_xoxo(&mut enc_msg_xoxo2, &primes);
+				let dec_msg_xoxo_bin = format!("{:b}", dec_msg_xoxo);
+				println!(">>> Decryted message: {}", dec_msg_xoxo_bin);
             } else {
                 eprintln!("Error while receiving the primes.");
             }
         },
         Err(e) => eprintln!("Error while receiving primes: {}", e),
     }
+
 }
 
 fn main() {
@@ -56,6 +71,7 @@ fn main() {
 	 */
 
     // Accept incoming connections in a loop
+	/*
     for stream in listener.incoming() {
         match stream {
             Ok(stream) => {
@@ -66,7 +82,15 @@ fn main() {
             }
             Err(e) => eprintln!("Error accepting connection: {}", e),
         }
-    }
+}
+	 */
+	if let Ok((stream, _)) = listener.accept() {
+		// Handle the client in a single thread
+		handle_client(stream);
+	} else {
+		eprintln!("Error accepting connection.");
+	}
+
 	return;
 	
 }
