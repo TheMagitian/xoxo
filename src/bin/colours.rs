9a60650812cade2620e994b2adf655eed58e0ea5
Magitian <magitian@duck.com>
2025-01-16 15:04:52 +0530

    Add colours file

---
+++ src/bin/colours.rs
@@ -0,0 +1,12 @@
+#![allow(dead_code)]
+pub const VIOLET: &str = "\x1B[35m";
+pub const BLUE: &str = "\x1B[34m";
+pub const YELLOW: &str = "\x1B[33m";
+pub const GREEN: &str = "\x1B[32m";
+pub const RED: &str = "\x1B[31m";
+pub const RESET: &str = "\x1B[0m";
+pub const BOLD: &str = "\x1B[1m\x1B[37m";
+pub const UNDERLINE: &str = "\x1B[1m\x1B[4m";
+pub const ITALIC: &str = "\x1B[3m\x1B[37m";
+pub const CYAN: &str = "\x1B[36m";
+pub const HIGHLIGHT: &str = "\x1B[1;37;48;2;165;42;42m";
